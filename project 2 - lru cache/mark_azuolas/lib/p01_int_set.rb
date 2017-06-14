86357b67419ded3cc6c8304cf51f5dc54afc3082
mark azuolas <markazuolas@gmail.com>
2017-06-13 18:10:28 -0700

    resizing int set

--- project 2 - lru cache/mark_azuolas/lib/p01_int_set.rb
+++ project 2 - lru cache/mark_azuolas/lib/p01_int_set.rb
@@ -62,23 +62,29 @@ class ResizingIntSet
   attr_reader :count
 
   def initialize(num_buckets = 20)
-    @store = Array.new(num_buckets) { Array.new }
+    @store = Array.new(num_buckets) { [] }
     @count = 0
   end
 
   def insert(num)
+    @count += 1
+    resize! if @count > num_buckets
+    self[num] << num if !self[num].include?(num)
   end
 
   def remove(num)
+    self[num].delete(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -86,5 +92,12 @@ class ResizingIntSet
   end
 
   def resize!
+    newStore = Array.new(num_buckets*2) {[]}
+    @store.each do |bucket|
+      bucket.each do |num|
+        newStore[num % newStore.length] << num
+      end
+    end
+    @store = newStore
   end
 end
