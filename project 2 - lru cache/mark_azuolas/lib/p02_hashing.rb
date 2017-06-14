d0112044e659a15561451a1295a9b1f4a19b04ed
mark azuolas <markazuolas@gmail.com>
2017-06-14 07:49:41 -0700

    hashing

--- project 2 - lru cache/mark_azuolas/lib/p02_hashing.rb
+++ project 2 - lru cache/mark_azuolas/lib/p02_hashing.rb
@@ -4,11 +4,21 @@ end
 
 class Array
   def hash
+    sum = 0
+    each_with_index do |el, i|
+      sum += el.hash * i+1
+    end
+    sum.hash
   end
 end
 
 class String
   def hash
+    sum = 0
+    split('').each_with_index do |char, i|
+      sum += char.ord.hash * i+1
+    end
+    sum.hash
   end
 end
 
@@ -16,6 +26,10 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    sum = 0
+    each_key do |key|
+      sum += key.hash + self[key].hash
+    end
+    sum.hash
   end
 end
