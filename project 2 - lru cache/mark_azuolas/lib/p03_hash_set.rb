837cc606364d59c5597f5530db8c3c555a7e6c5f
mark azuolas <markazuolas@gmail.com>
2017-06-14 07:54:58 -0700

    hash set

--- project 2 - lru cache/mark_azuolas/lib/p03_hash_set.rb
+++ project 2 - lru cache/mark_azuolas/lib/p03_hash_set.rb
@@ -9,18 +9,24 @@ class HashSet
   end
 
   def insert(key)
+    @count += 1
+    resize! if @count == num_buckets
+    self[key] << key unless self[key].include?(key)
   end
 
   def include?(key)
+    self[key].include?(key)
   end
 
   def remove(key)
+    self[key].delete(key)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num.hash % num_buckets]
   end
 
   def num_buckets
@@ -28,5 +34,12 @@ class HashSet
   end
 
   def resize!
+    newStore = Array.new(num_buckets*2) {[]}
+    @store.each do |bucket|
+      bucket.each do |num|
+        newStore[num.hash % newStore.length] << num
+      end
+    end
+    @store = newStore
   end
 end
