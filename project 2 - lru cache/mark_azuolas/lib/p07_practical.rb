ed0d96fcacc7619729f43620aa5f8fd3baadacda
mark azuolas <markazuolas@gmail.com>
2017-06-14 10:36:50 -0700

    can be pamindrome?

--- project 2 - lru cache/mark_azuolas/lib/p07_practical.rb
+++ project 2 - lru cache/mark_azuolas/lib/p07_practical.rb
@@ -1,5 +1,11 @@
 require_relative 'p05_hash_map'
 
-def can_string_be_palindrome?(string) 
-  
+def can_string_be_palindrome?(string)
+  letters = {}
+  string.each_char { |char| letters[char] ? letters[char] += 1 : letters[char] = 1 }
+  odds = 0
+  letters.each_value do |val|
+    odds += 1 if val.odd?
+  end
+  odds < 2
 end
