5a25aa086cab8ee1a543c9cb55966036f8e164d4
mark azuolas <markazuolas@gmail.com>
2017-06-15 10:38:34 -0700

    k largest nlogn

--- project-3-/mark_azuolas/lib/k_largest_elements.rb
+++ project-3-/mark_azuolas/lib/k_largest_elements.rb
@@ -1,5 +1,13 @@
 require_relative 'heap'
 
 def k_largest_elements(array, k)
-
+  heap = BinaryMinHeap.new { |x,y| y <=> x }
+  array.each do |el|
+    heap.push(el)
+  end
+  biggest = []
+  k.times do
+    biggest << heap.extract
+  end
+  biggest
 end
