584b3dfbdf8b78a2fcf217c37e1b532ddf42e5a4
mark azuolas <markazuolas@gmail.com>
2017-06-19 13:58:22 -0700

    bst

--- project-5/mark_azuolas/lib/binary_search_tree.rb
+++ project-5/mark_azuolas/lib/binary_search_tree.rb
@@ -13,7 +13,7 @@ class BinarySearchTree
       @root = BSTNode.new(value)
       return root
     end
-    
+
     node = @root
     while node
       last_node = node
@@ -24,22 +24,56 @@ class BinarySearchTree
   end
 
   def find(value, tree_node = @root)
+    return nil unless tree_node
+    return tree_node if value == tree_node.value
+    if value < tree_node.value
+      find(value, tree_node.left)
+    else
+      find(value, tree_node.right)
+    end
   end
 
   def delete(value)
+    node = @root
+    while node && node.value != value
+      last_node = node
+      node = check_children(value, node)
+    end
+    if node.left && node.right
+      max = maximum(node.left)
+      delete(max.value)
+      node.value = max.value
+    else
+      remove_node(last_node, node)
+    end
   end
 
   # helper method for #delete:
   def maximum(tree_node = @root)
+    return nil unless tree_node
+    unless tree_node.right
+      tree_node
+    else
+      maximum(tree_node.right)
+    end
   end
 
   def depth(tree_node = @root)
+    return 0 unless tree_node && (tree_node.left || tree_node.right)
+    left = 1 + depth(tree_node.left)
+    right = 1 + depth(tree_node.right)
+    left > right ? left : right
   end
 
   def is_balanced?(tree_node = @root)
+    (depth(tree_node.left) - depth(tree_node.right)).abs < 1
   end
 
   def in_order_traversal(tree_node = @root, arr = [])
+    return arr unless tree_node
+    arr = in_order_traversal(tree_node.left, arr)
+    arr += [tree_node.value]
+    in_order_traversal(tree_node.right, arr)
   end
 
 
@@ -50,6 +84,18 @@ class BinarySearchTree
       node.left
     else
       node.right
+    end
+  end
+
+  def remove_node(parent, child)
+    if child == @root
+      @root = nil
+      return
+    end
+    if parent.value > child.value
+      parent.left = child.left || child.right
+    else
+      parent.right = child.left || child.right
     end
   end
 end
--- project-5/mark_azuolas/lib/bst_practical.rb
+++ project-5/mark_azuolas/lib/bst_practical.rb
@@ -1,3 +1,33 @@
-
+require 'byebug'
 def kth_largest(tree_node, k)
+  # nodes = in_order_traversal(tree_node)
+  # find(nodes[nodes.length-k], tree_node)
+  kth_biggest(tree_node, k).pop
+end
+#
+# def in_order_traversal(tree_node = @root, arr = [])
+#   return arr unless tree_node
+#   arr = in_order_traversal(tree_node.left, arr)
+#   arr += [tree_node.value]
+#   in_order_traversal(tree_node.right, arr)
+# end
+#
+# def find(value, tree_node = @root)
+#   return nil unless tree_node
+#   return tree_node if value == tree_node.value
+#   if value < tree_node.value
+#     find(value, tree_node.left)
+#   else
+#     find(value, tree_node.right)
+#   end
+# end
+
+def kth_biggest(tree_node, k, arr = [])
+  return arr if k == arr.length
+  return arr unless tree_node
+  arr = kth_biggest(tree_node.right, k, arr)
+  return arr if k == arr.length
+  arr += [tree_node]
+  return arr if k == arr.length
+  kth_biggest(tree_node.left, k, arr)
 end
